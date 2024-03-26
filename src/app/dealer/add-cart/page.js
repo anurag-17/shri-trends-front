6b440ef9db767fe3b00d3d06af4b48f692f0948a
anurag pandey <anurag.pandey17@gmail.com>
2024-03-26 15:29:09 +0530

    remove

--- src/app/dealer/add-cart/page.js
+++ src/app/dealer/add-cart/page.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Addtocart from '@/components/dealer-dashboard/dashboard/dashboard-pages/products/Addtocart'
-import Cart from '@/components/dealer-dashboard/dashboard/dashboard-pages/products/Cart'
 const page = () => {
   return (
     <>
